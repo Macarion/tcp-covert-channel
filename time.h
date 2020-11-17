59a72a2c851569c3999b593365d2e4bf210fa63e
Macarion <3138842203@qq.com>
2020-11-17 23:07:16 +0800

    rebuild

---
+++ time.h
@@ -0,0 +1,16 @@
+#ifndef __TIME_H
+#define __TIME_H
+
+#include <linux/ktime.h>
+#include <linux/module.h>
+
+#define __CST (8 * 3600)
+
+MODULE_LICENSE("GPL");
+
+struct tm* getDateAndTime(struct tm *dtm);
+char* saveTimeToStr(char *str, const struct tm *time);
+
+#include "time.c"
+
+#endif
